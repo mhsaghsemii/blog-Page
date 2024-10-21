6c4d9df6d66b2f24439abb6b6659055314fe3d71
MAHSA <151853553+mhsaghsemii@users.noreply.github.com>
2024-10-21 14:18:22 +0330

    feat(Query Selector): Definition html elements in JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const title = document.querySelector("#blog-title");
+const descryption = document.querySelector(".descryption");
+const date = document.querySelector(".time");
+const author = document.querySelector(".author");
+const category = document.querySelector(".tag");
+const like = document.querySelector(".like");
+const comment = document.querySelector(".blog-comments");
